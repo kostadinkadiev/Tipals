d169f4b18cf5d6c300ab96182bee598a1167af25
kostadin.kadiev <kostadinkadiev@gmail.com>
2017-09-30 17:36:59 +0200

    starting over

--- Tipals/src/Tipals.Domain/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
-namespace Tipals.Domain.Entities
-{
-    public class User : IdentityUser
-    {
-        public Player Player { get; set; }
-    }
-}
